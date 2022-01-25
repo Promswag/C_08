03d5bd22908887c3cab6a09eabb00de579a7b7e8
Guillaume Baumgartner <gbaumgar@k0r2p13.42mulhouse.fr>
2022-01-25 15:01:50 +0100

    xd

---
+++ ex04/ft_strs_to_tab.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbaumgar <gbaumgar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 12:06:17 by gbaumgar          #+#    #+#             */
+/*   Updated: 2022/01/25 14:58:03 by gbaumgar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stock_str.h"
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int		i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str	*tab;
+	int			i;
+
+	tab = malloc(sizeof(t_stock_str) * ac);
+	i = 0;
+	while (i < ac)
+	{
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].str = av[i];
+		tab[i].copy = malloc(sizeof(char) * tab[i].size);
+		tab[i].copy = ft_strcpy(tab[i].copy, av[i]);
+		i++;
+	}
+	return (tab);
+}
